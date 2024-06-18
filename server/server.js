bc29fa5bfc35152d13a4ec20e148d84479990763
techmannih <manishchaudharyttt@gmail.com>
2024-06-18 19:47:36 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -4,22 +4,32 @@ const connectDB = require("./config/db");
 const movieRoutes = require("./routes/movieRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const cors = require('cors');
+
+// Load environment variables from .env file
 dotenv.config();
+
+// Connect to the database
 connectDB();
 
 const app = express();
 
 app.use(express.json()); // Parse incoming request bodies as JSON
 app.use(cors()); // Enable CORS for all routes
-app.use("/api/movies", movieRoutes); // Use movieRoutes for '/api/movies' endpoint
 
-app.use(notFound); // Handle 404 errors
-app.use(errorHandler); // Centralized error handler
+// Use movieRoutes for '/api/movies' endpoint
+app.use("/api/movies", movieRoutes);
 
+// Simple response for root endpoint
 app.get("/", (req, res) => {
-  res.send("Hello World"); // Simple response for root endpoint
+  res.send("Hello World");
 });
+
+// Handle 404 errors
+app.use(notFound);
+
+// Centralized error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
